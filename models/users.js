4884acab56a0f92452651bf6a3f93759449e91e6
Sanket Apte <sanket.a@turtlemint.com>
2017-02-19 19:24:09 +0530

    added an API to create new user

--- models/users.js
+++ models/users.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 
 var usersSchema = new mongoose.Schema({
-	_id: String, 
-	username : String,
-	emailId : String,
-	firstName: String,
-	lastName : String,
-	userType : String,
-	mobileNo : Number,
-	landlineNo : Number,
-	isActive: Boolean,
+	_id: { type : String, required : true},
+	username : { type : String, required : false},
+	emailId : { type : String, required : false},
+	firstName: { type : String, required : false},
+	lastName : { type : String, required : false},
+	userType : { type : String, required : false},
+	mobileNo : { type : Number, required : false},
+	landlineNo : { type : Number, required : false},
+	isActive: { type : Boolean, required : false},
 	createdAt: { type: Date, default: Date.now },
 	updatedAt: { type: Date, default: Date.now }
 });
--- routes/travellers.js
+++ routes/travellers.js
@@ -12,6 +12,23 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+router.post('/', function(req, res, next) {
+	var user = req.body;
+	var newUser = new Users(user);
+	newUser.save(function(err, users){
+		if(err)
+			return next(err);
+		res.json(users);
+	});
+	/*
+	Users.create(req.body, function(err, users) {
+		if(err)
+			return next(err);
+		res.json(users);
+	});
+	*/
+});
+
 /* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
